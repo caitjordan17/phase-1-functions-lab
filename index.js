8eaaede75d5c83dd191751d9fda5266da4e6eacd
Caitlin Jordan <jordan.cait17@gmail.com>
2023-01-19 20:58:53 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation){
+    const headquarters = 42;
+    if (pickUpLocation > headquarters){
+        return (pickUpLocation - headquarters);
+    } else if (pickUpLocation < headquarters){
+        return (headquarters - pickUpLocation);
+    }
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    return distanceFromHqInBlocks(pickUpLocation) * 264;
+  }
+
+function distanceTravelledInFeet(pickUpLocation, dropOffLocation){
+    return distanceFromHqInFeet(dropOffLocation) - distanceFromHqInFeet(pickUpLocation);
+}
+
+function calculatesFarePrice(pickUpLocation, dropOffLocation){
+    let feet = distanceTravelledInFeet(pickUpLocation, dropOffLocation)
+    if (feet > 2500){
+        return 'cannot travel that far';
+    }
+    else if (feet > 2000){
+        return 25;
+    }
+    else if (feet > 400){
+        return (feet - 400) * 0.02;
+    }
+    else if (feet < 400){
+        return 0;
+    }
+}
